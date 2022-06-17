3c1397bd126edc0788fba62b312f632682057e49
Song San <songsan2012@gmail.com>
2022-06-16 23:48:45 -0700

    Add the datasource and delegates

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController { 
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
@@ -22,13 +23,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+//        currencyPicker . UIPickerViewDataSource = self
+//        UIPickerViewDelegate = self
+
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+         return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
     
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        print(row)
+        print(currencyArray[row])
+        
+        finalURL = baseURL + currencyArray[row]
+        print("FinalURL is \(finalURL)")
+    }
     
 
     
